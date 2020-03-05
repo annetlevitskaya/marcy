f869e69b44da6540ef777561a59bb075d7b00c8e
annetlevitskaya <annetlevitskaya@gmail.com>
2020-03-05 11:03:43 +0200

    add build

---
+++ build/precache-manifest.523ae19440f9cdadf11e0c3676f23d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a90c302c1745e2863a7edd151f9cff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7333ebae97f1e61fc69e",
+    "url": "/static/css/main.40debc3a.chunk.css"
+  },
+  {
+    "revision": "ea72de219c967bbd11bb",
+    "url": "/static/js/2.46131025.chunk.js"
+  },
+  {
+    "revision": "7333ebae97f1e61fc69e",
+    "url": "/static/js/main.c5297e44.chunk.js"
+  },
+  {
+    "revision": "5f7165e5903a1e6c1482",
+    "url": "/static/js/runtime-main.6e420b96.js"
+  },
+  {
+    "revision": "8877398a1ec59053b399f9deda645cfe",
+    "url": "/static/media/forest.8877398a.png"
+  },
+  {
+    "revision": "25d040445b9f4c943f5f14fd545e86d5",
+    "url": "/static/media/video.25d04044.png"
+  }
+]);
